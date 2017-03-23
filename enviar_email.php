1d6b70e2ce1a518f9c7ce6282eea2f5239e9a617
Marcus Oliveira <marcusoliveirapc@gmail.com>
2017-03-22 21:04:05 -0300

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -4,9 +4,9 @@ $nome=$_POST['id_nome'];
 $assunto=$_POST['id_assunto'];
 $mensagem=$_POST['id_mensagem'];
 
-mail("marcusoliveirapc@gmail.com", $assunto, $nome . ':' . $mensagem);
+mail("exemplo@bol.com.br", $assunto, $nome . ':' . $mensagem);
 
-header("location:https://developer404.000webhostapp.com/contact.html");
+header("location:sua pagina de contato . exemplo https://developer4044040.000webhostapp.com/contact.html");
 
 
 ?>
